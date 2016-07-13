f8f1e69328456c71ba9fe7c7dc03cb08919b92e1
yzbbanban <yzbbanban@live.com>
2016-07-14 00:52:43 +0800

    添加开篇动画
    准备修改自己的图标

---
+++ app/src/main/java/com/wangban/yzbbanban/banmusicplayer/util/FloatWrapper.java
@@ -0,0 +1,14 @@
+package com.wangban.yzbbanban.banmusicplayer.util;
+
+/**
+ * Created by rahul.raja on 9/26/15.
+ */
+public class FloatWrapper {
+
+    public FloatWrapper(Float floatVal) {
+
+        this.floatValue = floatVal;
+    }
+
+    public Float floatValue;
+}
